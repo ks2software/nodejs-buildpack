9b26bde3709c849afdaca3900f41623cde4fc83f
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 15:05:20 -0500

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,24 +9,3 @@ export GOOS=linux
 
 go build -ldflags="-s -w" -o $BINDIR/supply nodejs/supply/cli
 go build -ldflags="-s -w" -o $BINDIR/finalize nodejs/finalize/cli
-
-# Save current path?
-MYPATH=$PWD
-
-# Download oracle 
-wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-basic-linux.x64-12.2.0.1.0.zip" -P $BINDIR
-wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-sdk-linux.x64-12.2.0.1.0.zip" -P $BINDIR
-
-# Install oracle
-cd $BINDIR
-unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
-unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip
-mv instantclient_12_2 instantclient
-cd instantclient
-ln -s libclntsh.so.12.1 libclntsh.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-export OCI_LIB_DIR=$PWD
-export OCI_INC_DIR=$PWD/sdk/include
-echo $OCI_LIB_DIR
-echo $OCI_INC_DIR
-cd $MYPATH
---
+++ scripts/install_oracle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Download oracle 
+wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-basic-linux.x64-12.2.0.1.0.zip" -P $BINDIR
+wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-sdk-linux.x64-12.2.0.1.0.zip" -P $BINDIR
+# Install oracle
+cd $BINDIR
+unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
+unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip
+mv instantclient_12_2 instantclient
+cd instantclient
+ln -s libclntsh.so.12.1 libclntsh.so
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+export OCI_LIB_DIR=$PWD
+export OCI_INC_DIR=$PWD/sdk/include
+echo $OCI_LIB_DIR
+echo $OCI_INC_DIR
