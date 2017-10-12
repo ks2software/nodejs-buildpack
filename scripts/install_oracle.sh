42009b5b7dc8e6a195c39ff08f57d879f85132cb
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 17:23:58 -0500

    Updated build script

--- scripts/install_oracle.sh
+++ scripts/install_oracle.sh
@@ -2,8 +2,7 @@
 # Download oracle 
 set -ex
 
-ROOTDIR="/opt"
-mkdir $ROOTDIR/oracle
+ROOTDIR="/home/vcap/app"
 BINDIR=$ROOTDIR
 wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-basic-linux.x64-12.2.0.1.0.zip" -P $BINDIR
 wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-sdk-linux.x64-12.2.0.1.0.zip" -P $BINDIR
