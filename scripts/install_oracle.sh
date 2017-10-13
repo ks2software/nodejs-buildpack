9e766a00e2711ed98f104d5694440fa7b7a26d2a
Dan Jarvis <danwakeem@gmail.com>
2017-10-12 19:13:15 -0500

    Libaio

--- scripts/install_oracle.sh
+++ scripts/install_oracle.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Download oracle 
 set -ex
+ROOTDIR="/home/vcap/deps"
 
-sudo apt-get install libaio1
+export BUILDPACK_DIR=`dirname $(readlink -f ${BASH_SOURCE%/*})`
+cd $BUILDPACK_DIR/libaio
+make prefix=$ROOTDIR install
+ls -la
 
-ROOTDIR="/home/vcap/app"
 BINDIR=$ROOTDIR
 wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-basic-linux.x64-12.2.0.1.0.zip" -P $BINDIR
 wget --no-check-certificate --no-proxy "https://s3.amazonaws.com/cityofdenton-lib/instantclient-sdk-linux.x64-12.2.0.1.0.zip" -P $BINDIR
@@ -16,7 +19,7 @@ mv instantclient_12_2 instantclient
 cd instantclient
 cp libclntsh.so.12.1 libclntsh.so
 # ln -s libclntsh.so.12.1 libclntsh.so
-export LD_LIBRARY_PATH=$PWD:
+export LD_LIBRARY_PATH=$ROOTDIR:
 export OCI_LIB_DIR=$PWD
 export OCI_INC_DIR=$PWD/sdk/include
 echo $OCI_LIB_DIR
